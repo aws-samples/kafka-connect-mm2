1c82cc33a966481e66041f97b68aa95f817ead04
andharma <andharma@88e9fe6f2521.ant.amazon.com>
2021-05-24 17:18:57 -0400

    first version

---
+++ connect-distributed-docker/start-kafka-connect.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Strating Kafka connect
+
+echo -e "Current file contents:\n $(cat /etc/hosts)"
+echo "$DETECTED_IP $DETECTED_HOSTNAME" >> /etc/hosts
+echo -e "\n\n\nUpdated file contents:\n $(cat /etc/hosts)"
+
+echo $BROKERS $USERNAME $GROUP
+sed -i "s/BROKERS/${BROKERS}/g" /opt/connect-distributed.properties
+sed -i "s/GROUP/${GROUP}/g" /opt/connect-distributed.properties
+sed -i "s/USERNAME/${USERNAME}/g" /opt/connect-distributed.properties
+sed -i "s/PASSWORD/${PASSWORD}/g" /opt/connect-distributed.properties
+
+echo Starting Kafka connect
+
+cd /opt/kafka_2.13-2.7.0/bin
+./connect-distributed.sh /opt/connect-distributed.properties
+
+
+while :
+do
+	sleep 20
+done
